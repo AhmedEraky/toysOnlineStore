0f3969d7a6b66f27c6e06d5906f7b8a976851481
IslamElrougy <islamelrougy@gmail.com>
2019-03-28 18:26:38 +0200

    Implemented AdminDaoImplementation

--- src/main/java/com/iti/Dao/implementation/AdminDaoImplementation.java
+++ src/main/java/com/iti/Dao/implementation/AdminDaoImplementation.java
@@ -2,26 +2,47 @@ package com.iti.Dao.implementation;
 
 import com.iti.Dao.AdminDao;
 import com.iti.model.entity.Admin;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
-public class AdminDaoImplementation implements AdminDao {
+public class AdminDaoImplementation implements AdminDao
+{
+
     @Override
-    public boolean persistAdmin(Admin admin, Session session) {
-        return false;
+    public boolean persistAdmin(Admin admin, Session session)
+    {
+        try
+        {
+            session.beginTransaction();
+            session.persist(admin);
+            session.getTransaction().commit();
+            return true;
+        } catch (HibernateException ex)
+        {
+            session.getTransaction().rollback();
+            return false;
+        }
     }
 
     @Override
-    public Admin retiveAdminByEmail(String email, Session session) {
-        return null;
+    public Admin retrieveAdminByEmail(String email, Session session)
+    {
+        Admin admin = session.get(Admin.class, email);
+        return admin;
+
     }
 
     @Override
-    public Admin retriveAdminByExample(Admin admin, Session session) {
-        return null;
-    }
-
-    @Override
-    public boolean isAdmin(Admin admin, Session session) {
-        return false;
+    public boolean isAdmin(Admin admin, Session session)
+    {
+        Admin testAdmin = session.get(Admin.class, admin.getEmail());
+        if (testAdmin == null)
+        {
+            return false;
+        } 
+        else
+        {
+            return true;
+        }
     }
 }
