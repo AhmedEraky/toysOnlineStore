da7f9c907a5b911a19f27d6c0a95023c7a90d26d
“eraky” <ahmedmoeray@gmail.com>
2019-03-31 17:36:12 +0200

    add user to session

--- src/main/java/com/iti/Main.java
+++ src/main/java/com/iti/Main.java
@@ -14,34 +14,8 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String arg[]){
 
-        UserController userController=new UserController();
-
-        User user=new User();
-        ShoppingCart shoppingCart=new ShoppingCart();
-
-        user.setEmail("ahmed@yahoo.com");
-        user.setAddress("Cairo");
-        user.setBirthDate(new Date());
-        user.setCreditLimit(10d);
-        user.setName("Ahmed Eraky");
-        user.setPassword("111111");
-        shoppingCart.setTotalCost(0d);
-
-        user.setShoppingCart(shoppingCart);
-        userController.insertUser(user);
-        User currentUser=userController.retriveUserByEmail("ahmed@yahoo.com");
-        System.out.println("current Job is "+currentUser.getJob());
-        User newu=new User();
-        newu.setJob("new Job");
-        newu.setName("fdut");
-        userController.updateUser(currentUser,newu);
-        user=userController.retriveUserByEmail("ahmed@yahoo.com");
-        System.out.println("new Data = "+user.getJob());
-
-        Session session = HibernateUtils.getSession();
-        Category category = new Category("Dolls");
-        CategoryDaoImplementation categoryDaoImplementation = new CategoryDaoImplementation();
-        categoryDaoImplementation.persistCategory(category, session);
-        System.out.println("Done");
+       Session session=HibernateUtils.getSession();
+       session.beginTransaction();
+       session.getTransaction().commit();
     }
 }
