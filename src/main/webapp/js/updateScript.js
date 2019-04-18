e1b55d6a126560e9f8e7af63c5da12e58f8e8922
Hadeer <hadeer.ayman1994@gmail.com>
2019-04-18 09:30:08 +0200

    update profile validation

--- src/main/webapp/js/updateScript.js
+++ src/main/webapp/js/updateScript.js
@@ -19,6 +19,15 @@ document.getElementById("edit").addEventListener('click',function() {
         if (credit.value < 0){
             credit.style="border-color: #E34234;";
             document.getElementById("invalidCredit").innerText = "invalid entry";
+            console.log(address.value=="");
+            console.log(job.value=="");
+            console.log(birthDate.value=="");
+            console.log(credit.value=="");
+
+        }
+        else if (birthDate.value=="" || job.value=="" || address.value=="" || credit.value==""){
+            console.log("///////////////////");
+            document.getElementById("empty").innerText = "There's some Empty Data must be filled";
         }
         else {
             document.getElementById("profileForm").submit();
