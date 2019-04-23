d75f9d5f29c23de78a398b02bebc3d89278a0267
AhmedEraky <ahmed_eraky95@yahoo.com>
2019-04-23 10:39:20 +0200

    crate DB

--- src/main/java/com/iti/controller/listner/ContextListner.java
+++ src/main/java/com/iti/controller/listner/ContextListner.java
@@ -2,16 +2,23 @@ package com.iti.controller.listner;
 
 import com.iti.model.Dao.AdminDao;
 import com.iti.model.Dao.CategoryDao;
+import com.iti.model.Dao.ProductDao;
+import com.iti.model.Dao.StoreDao;
 import com.iti.model.Dao.implementation.AdminDaoImplementation;
 import com.iti.model.Dao.implementation.CategoryDaoImplementation;
+import com.iti.model.Dao.implementation.ProductDaoImplementation;
+import com.iti.model.Dao.implementation.StoreDaoImplementation;
 import com.iti.model.cfg.HibernateUtils;
 import com.iti.model.cfg.transaction.TransactionManager;
 import com.iti.model.entity.Admin;
 import com.iti.model.entity.Category;
+import com.iti.model.entity.Product;
+import com.iti.model.entity.Store;
 import org.hibernate.Session;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.List;
 
 public class ContextListner implements ServletContextListener {
     int imageCount;
@@ -28,12 +35,67 @@ public class ContextListner implements ServletContextListener {
 
                 createAdmins(session);
                 createCategories(session);
+                createStores(session);
+
+
+                StoreDao storeDao=new StoreDaoImplementation();
+                Store store1=storeDao.retrieveStoreByName("Eraky Store",session);
+                Store store2=storeDao.retrieveStoreByName("Aya Store",session);
+                Store store3=storeDao.retrieveStoreByName("Islam Store",session);
+
+                CategoryDao categoryDao=new CategoryDaoImplementation();
+                Category category1=categoryDao.retriveCategoryByName("Action Figures",session);
+                Category category2=categoryDao.retriveCategoryByName("Cars And Planes",session);
+                Category category3=categoryDao.retriveCategoryByName("Construction",session);
+                Category category4=categoryDao.retriveCategoryByName("Dolls",session);
+                Category category5=categoryDao.retriveCategoryByName("Puzzles",session);
+
+
+                createActionFiguresProduct(session,store1,category1);
 
                 return null;
             });
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void createStores(Session session) {
+        Store store=new Store();
+        store.setName("Eraky Store");
+        StoreDao storeDao=new StoreDaoImplementation();
+
+        Store store1=new Store();
+        store1.setName("Aya Store");
+
+        Store store2=new Store();
+        store2.setName("Islam Store");
+
+        storeDao.persistStore(store,session);
+        storeDao.persistStore(store1,session);
+        storeDao.persistStore(store2,session);
+
+    }
+
+    private void createActionFiguresProduct(Session session,Store store,Category category) {
+        Product product1=new Product();
+        product1.setName("Fly");
+        product1.setMinAge(3);
+        product1.setPurchaseCount(0);
+        product1.setDiscountPercentage(10);
+        product1.setPrice(200d);
+        product1.setImagePath("images/products/actionfigures/01/bighero.jpg");
+        product1.setQuantity(30);
+        product1.setDescription("Good Eraky Product");
+        product1.setStore(store);
+        product1.setCategory(category);
+
+
+        ProductDao productDao=new ProductDaoImplementation();
+        productDao.persistProduct(product1,session);
+
+
+
     }
 
     private void createCategories(Session session) {
